a3ea21ddbe8d176bdb2351606dc2ecc025bca27d
Ayman Soliman <49342013+AymanMagdy@users.noreply.github.com>
2020-11-20 00:02:04 +0200

    Changing new DP

--- Functions, Methods, and Interfaces In Go/Week 4/animal_interface.go
+++ Functions, Methods, and Interfaces In Go/Week 4/animal_interface.go
@@ -1,189 +1,70 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-// The Named interface describes any entity that can have a name
-type Named interface {
-	getName() string
+// The animal type with the needed strings needed
+type animal struct {
+	food 	  string
+	locmotion string
+	noise     string	
 }
 
-// The animal interface, prints the animal eat, move and sound
-type Animal interface {
-	Named
+// The animal interface with the needed methods.
+type animalInterface interface {
 	Eat()
 	Move()
 	Speak()
 }
 
-// The Cow type
-type Cow struct {
-	name string
+// The Eat, Move and Speak methods
+func (ani animal) Eat() {
+	fmt.Println(ani.food)
+	return
 }
 
-// The Bird type
-type Bird struct {
-	name string
+func (ani animal) Move() {
+	fmt.Println(ani.locmotion)
+	return
 }
 
-type Snake struct {
-	name string
-}
-
-// The Named interface with the needed methods for the animals.
-
-// The cow methods for returning the name
-func (cow Cow) getName() string {
-	return cow.getName()
-}
-
-// The bird method for returning the name
-func (bird Bird) getName() string {
-	return bird.getName()
-}
-
-// The snake method for returning the name
-func (snake Snake) getName() string {
-	return snake.getName()
+func (ani animal) Speak() {
+	fmt.Println(ani.noise)
+	return
 }
 
 
-// The Eat method for the different types of animals {cow, bird and snake}
-func (cow Cow) Eat() {
-	fmt.Println("Grass")
-}
-
-func (bird Bird) Eat() {
-	fmt.Println("Worms")
-}
-
-func (snake Snake) Eat() {
-	fmt.Println("Mice")
-}
-
-// The Move method for the different types of animals {cow, bird and snake}
-func (cow Cow) Move() {
-	fmt.Println("Walk")
-}
-
-func (bird Bird) Move() {
-	fmt.Println("Fly")
-}
-
-func (snake Snake) Move() {
-	fmt.Println("Slither")
-}
-
-// The Speak method for the different types of animals {cow, bird, snake}
-func (cow Cow) Speak() {
-	fmt.Println("Moo")
-}
-
-func (bird Bird) Speak() {
-	fmt.Println("Peep")
-}
-
-func (snake Snake) Speak() {
-	fmt.Println("Hsss")
-}
-
-// Checking the input from the user
-func isValidCommand(animal string) bool {
-	return command == "newanimal" || command == "query"
-}
-
-func isValidAnimal(animal string) bool {
-	return animal == "cow" || animal == "bird" || animal == "snake"
-}
-
-func isValidAnimalProperty(property string) bool {
-	return property == "eat" || property == "move" || property == "speak"
-}
-
-func getRequest() (string, string, string) {
-	var command, name, option string
-	for {
-		fmt.Print(">")
-		if _, err := fmt.Scanln(&command, &name, &option); err != nil {
-			fmt.Println("Error:", err)
-		} else {
-			if isValidCommand(command) {
-				if command == "newanimal" {
-					if isValidAnimal(animal) {
-						break
-					} else {
-						fmt.Println("Invalid animal type. Please use 'cow', 'bird' or 'snake'.")
-					}
-				} else if command == "query" {
-					if isValidAnimalProperty(option) {
-						break
-					} else {
-						fmt.Println("Invalid animal property. Please use 'cow', 'bird' or 'snake'.")
-					}
-				}
-			} else {
-				fmt.Println("Invalid command. Please use 'newanimal' or 'query'.")
-			}
-		}
-	}
-
-	return command, name, option
-}
-
-// Appening a new animal type
-func appendAnimal(animals *[]Animal, animal Animal) {
-	*animals = append(*animals, animal)
-	fmt.Println("Created it!")
-}
-
-// Finding the animals with the given names.
-func findAnimal(animals []Animal, name string) (Animal, error) {
-	var foundAnimal Animal
-	var err = errors.New("Animal with that given name is not found.")
-	for _, animal := range animals {
-		if Named(animal).getName() == name {
-			foundAnimal = animal
-			err = nil
-			break
-		}
-	}
-
-	return foundAnimal, err
-}
-
-// The main function
 func main() {
-	var animals []Animal
+	animalMap := make(map[string]animal)
+
+	animalMap["cow"] = animal{"grass", "walk", "moo"}
+	animalMap["bird"] = animal{"worm", "fly", "peep"}
+	animalMap["snake"] = animal{"mice", "slither", "hss"}
+
+	var generalAnimal animalInterface
+
 	for {
-		command, name, option := getRequest()
-		switch command {
-		case "newAnimal":
-			switch option {
-			case "cow":
-				appendAnimal(*animals, Cow{name})
-			case "bird":
-				appendAnimal(*animals, Bird{name})
-			case "snake":
-				appendAnimal(*animals, Snake{name})
-			}
-		case "query":
-			var animal Animal
-			var err error
-			if Animal, err = findAnimal(animals, name); err != nil {
-				fmt.Println(err)
-				break
-			}
-			switch option {
+		var command, requestAnimal, requestType string
+		fmt.Print(">")
+		fmt.Scan(&command, &requestAnimal, &requestType)
+
+		if command == "query" {
+			generalAnimal = animalMap[requestAnimal]
+
+			switch requestType {
 			case "eat":
-				animal.Eat()
+				generalAnimal.Eat()
 			case "move":
-				animal.Move()
+				generalAnimal.Move()
 			case "speak":
-				animal.Speak()
-				
+				generalAnimal.Speak()
 			}
+		}
+
+		if command == "newanimal" {
+			animalMap[requestAnimal] = animalMap[requestType]
+			fmt.Println("Created a new animal")
 		}
 	}
 }
